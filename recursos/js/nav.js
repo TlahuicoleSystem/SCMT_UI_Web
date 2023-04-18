6bc40e88e55517c59f7a09212d678a02d89c7a05
Abraham Cocoletzi Z <abraham_cryter2007@hotmail.com>
2023-04-18 16:49:19 -0600

    Interfaz incidencias y modal del perfil

--- recursos/js/nav.js
+++
@@ -1,9 +0,0 @@
-let btn = document.querySelector("#btn");
-let sidebar = document.querySelector(".sidebar");
-
-btn.onclick = function () {
-    sidebar.classList.toggle("active");
-};
-srcBtn.onclick = function () {
-    sidebar.classList.toggle("active");
-};
