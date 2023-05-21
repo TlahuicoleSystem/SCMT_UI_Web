b51aaec2b5a90183380f4ac5e10cedadcefdacac
Abraham Cocoletzi Z <abraham_cryter2007@hotmail.com>
2023-05-20 21:25:02 -0600

    Implementando boton cerrar sesion y denegacion por navegacion por url

--- recursos/js/inicio.js
+++ recursos/js/inicio.js
@@ -1,37 +1,35 @@
+// Verificar si no hay datos en el sessionStorage
+if (
+    !sessionStorage.getItem("nombre") ||
+    !sessionStorage.getItem("descripcion") ||
+    !sessionStorage.getItem("foto") ||
+    !sessionStorage.getItem("telefono")
+) {
+    // Redirigir a la página de inicio de sesión (index.html)
+    window.location.href = "../index.html";
+}
+
 let btn = document.querySelector("#btn");
 let sidebar = document.querySelector(".sidebar");
 let nombre = sessionStorage.getItem("nombre"),
     descripcionn = sessionStorage.getItem("descripcion"),
     fotoo = sessionStorage.getItem("foto"),
     tel = sessionStorage.getItem("telefono");
-    document.getElementById("foto_perfil").src = fotoo;
-    document.getElementById("nombreMenu").innerHTML = `<p>Hola ${nombre}</p> <p>Descripcion: ${descripcionn}</p><p>Telefono: ${tel}</p>` 
-    document.getElementById("nombre_spam").innerHTML = `<img src="${fotoo}" alt="Imagen de perfil" class="img-fluid espaciado-img bx" width="50" height="50"> <span class="link_name">${nombre}</span>` 
+document.getElementById("foto_perfil").src = fotoo;
+document.getElementById("nombreMenu").innerHTML = `<p>Hola ${nombre}</p> <p>Descripcion: ${descripcionn}</p><p>Telefono: ${tel}</p>`
+document.getElementById("nombre_spam").innerHTML = `<img src="${fotoo}" alt="Imagen de perfil" class="img-fluid espaciado-img bx" width="50" height="50"> <span class="link_name">${nombre}</span>`
 
 btn.onclick = function () {
     sidebar.classList.toggle("active");
 };
-/*
-// Fecha
-var fechaActual = new Date(); // Obtiene la fecha actual
-var tdFecha = document.getElementById("fecha-actual_0"); // Selecciona el elemento td por su id
-var opciones1 = {
-    day: 'numeric',
-    month: 'numeric',
-    year: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
-    hour12: true
-};
-tdFecha.textContent = fechaActual.toLocaleString("es-MX", opciones1); // Escribe la fecha y hora en el elemento td
 
-var tdFecha = document.getElementById("fecha-actual_1");
-var opciones2 = {
-    day: 'numeric',
-    month: 'numeric',
-    year: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
-    hour12: true
-};
-tdFecha.textContent = fechaActual.toLocaleString("es-MX", opciones2);*/
+function cerrarSesion() {
+    // Limpiar los valores del sessionStorage
+    sessionStorage.removeItem("nombre");
+    sessionStorage.removeItem("descripcion");
+    sessionStorage.removeItem("foto");
+    sessionStorage.removeItem("telefono");
+
+    // Redirigir a la página de inicio de sesión
+    window.location.href = "../index.html";
+}
